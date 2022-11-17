228944b4735751be74e190433cdf4a7eea8ebe2a
ChNafees5 <chnafees54@gmail.com>
2022-11-17 06:59:03 +0500

    update commit

--- routers/product.js
+++ routers/product.js
@@ -21,4 +21,35 @@ router.put('/:id', verifyTokenAndAdmin, async(req, res) => {
       res.send(500).json(err)  
     }
 })
+router.delete('/:id', verifyTokenAndAdmin, async(req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(200).json('product has been deleted')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+router.get('/:id', async(req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        res.status(200).json(product)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/', async(req, res) => {
+    const qNew = req.query.new
+    const qCategory = req.query.category
+    try {
+        const users = await Product.find()
+        // const users = query ? await User.find().sort({_id:-1}).limits(5) : await User.find()
+         res.status(200).json(users)
+     
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
 module.exports = router
