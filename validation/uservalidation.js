6c266c75b59f690061f6431d5de65bce29e01701
ChNafees5 <chnafees54@gmail.com>
2022-12-05 19:42:17 +0500

    update

---
+++ validation/uservalidation.js
@@ -0,0 +1,9 @@
+
+import Joi from "@hapi/joi";
+
+//const name = Joi.string().max(255).required().label('Name');
+const authSchema=Joi.object({
+email : Joi.string().email().required(),
+username : Joi.string().max(255).min(3).required(),
+password : Joi.string().max(30).min(4).required(),
+})
